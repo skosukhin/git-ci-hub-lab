29b00bd270085f533a92bad5d3dc1148d30e28f4
Sergey Kosukhin <sergey.kosukhin@mpimet.mpg.de>
2024-08-15 12:29:31 +0200

    gl-cancel-pipeline: add --force argument

--- lib/cmd/gl-cancel-pipeline.py
+++ lib/cmd/gl-cancel-pipeline.py
@@ -1,3 +1,5 @@
+from common import info, warn
+
 description = "cancels a GitLab CI pipeline"
 
 
@@ -8,7 +10,16 @@ def setup_parser(parser):
     )
     parser.add_argument("--token", required=True, help="GitLab access token")
     parser.add_argument(
-        "--pipeline-id", required=True, help="ID of the pipeline"
+        "--pipeline-id",
+        required=True,
+        metavar="PIPELINE_ID",
+        help="ID of the pipeline",
+    )
+    parser.add_argument(
+        "--force",
+        action="store_true",
+        help="do not fail if PIPELINE_ID could not be cancelled "
+        "(default: '%(default)s')",
     )
 
 
@@ -25,4 +36,13 @@ def cmd(args):
     project = server.projects.get(args.project_name, lazy=True)
     pipeline = project.pipelines.get(args.pipeline_id, lazy=True)
 
-    pipeline.cancel()
+    try:
+        pipeline.cancel()
+        info(
+            "Pipeline '{0}' is successfully cancelled".format(args.pipeline_id)
+        )
+    except Exception:
+        if args.force:
+            warn("Failed to cancel pipeline '{0}'".format(args.pipeline_id))
+        else:
+            raise
